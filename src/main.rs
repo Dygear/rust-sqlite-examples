a68baabba3722181101f9b244d41ac83cb89b8f8
Mark Tomlin <Dygear@gmail.com>
2021-09-15 16:12:01 -0400

    Error state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 use rusqlite::{Result, Connection};
 
-#[derive(Debug)]
-struct SelectedRow
-{
-    pub PKValue: u64
-}
-
 fn main()
 {
     let sql = match Connection::open("SQLite.db")
@@ -22,6 +16,36 @@ fn main()
         }
     };
 
-    let PKValue: u64 = 1337;
-    let row: Result<SelectedRow, rusqlite::Error> = sql.query_row("SELECT * FROM table WHERE PKValue = :PKValue;", &[(":PKValue", &PKValue)], |row| row.get(0),);
+/*
+    sql.execute(r#"
+        CREATE TABLE example
+        (
+            pk INTEGER PRIMARY KEY,
+            key TEXT NOT NULL,
+            val TEXT DEFAULT NULL
+        );
+    "#, []);
+
+    sql.execute(r#"
+        INSERT INTO example (key, val) VALUES
+        ('0', 'Zero'),
+        ('1', 'One'),
+        ('2', 'Two'),
+        ('3', 'Three'),
+        ('4', 'Four'),
+        ('5', 'Five'),
+        ('6', 'Six'),
+        ('7', 'Seven'),
+        ('8', 'Eight'),
+        ('9', 'Nine'),
+        ('10', 'Ten'),
+        ('11', 'Eleven'),
+        ('12', 'Twelve');
+    "#, []);
+*/
+
+    let pkvalue: i64 = 1337;
+    let row: Result<String, rusqlite::Error> = sql.query_row("SELECT val FROM example WHERE pk = :pkvalue;", &[(":pkvalue", &pkvalue)], |row| row.get(0),);
+
+    println!("Value of row: {:?}", row);
 }
